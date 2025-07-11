bada02b843963253bc612a874f14ce6ba258757c
Ayan <dev.muhammadayan@gmail.com>
2025-07-12 02:04:31 +0500

    Final backend push

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const connect = async () => {
     }
 };
 
-app.use(cors({origin:"http://localhost:5173", credentials:true}));
+app.use(cors({origin:"https://fiverr-client-deployment.vercel.app/", credentials:true}));
 app.use(express.json());
 app.use(cookieParser());
 
